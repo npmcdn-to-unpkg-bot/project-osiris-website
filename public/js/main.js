6b6326209759b32b6e8c1df901aefcfa922377d7
HermanFassett <hermanfassett@gmail.com>
2016-01-27 20:19:33 -0800

    Remove item padding and image border

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
+  // Set title preview
   $("#title").on('keyup', function(e) {
     $("#title-preview").text($("#title").val());
     $("#title-preview").attr("href", $("#url").val());
   })
+  // Set image preview
   $("#url").on('keyup', function(e) {
     $("#img-preview").attr("src", $("#url").val());
   })
@@ -10,12 +12,14 @@ $(document).ready(function() {
   $("#img-preview").error(function() {
     $(this).attr("src", "http://dummyimage.com/800x600&text=Pinternet");
   });
+  // Show grid
   var $grid = $('.grid').masonry({
     itemSelector: '.grid-item',
     fitWidth: true,
     transitionDuration: '0.8s',
     columnWidth: 5
   });
+  // Set layout so no image overlap on image load
   $grid.imagesLoaded().progress( function() {
     $grid.masonry('layout');
   });
