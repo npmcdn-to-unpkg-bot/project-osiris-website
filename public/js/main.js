954df885c95d0f93c954434270153900cf6244f5
Herman Fassett <hermanfassett@gmail.com>
2016-05-09 18:41:32 +0000

    Change encounter style width

--- public/js/main.js
+++ public/js/main.js
@@ -4,33 +4,9 @@ $(document).ready(function() {
     $("#title-preview").text($("#title").val());
     $("#title-preview").attr("href", $("#url").val());
   })
-  // Set image preview
-  $("#url").on('keyup', function(e) {
-    $("#img-preview").attr("src", $(this).val());
-  })
-  // Replace broken images
-  $(".grid-img").error(function() {
-    $(this).attr("src", "http://dummyimage.com/800x600&text=Pinternet");
-  });
   // Show grid
   var $grid = $('.grid').masonry({
     itemSelector: '.grid-item',
-    fitWidth: true,
-    transitionDuration: '0.8s',
-    columnWidth: 10
+    columnWidth: 80
   });
-  // Set layout so no image overlap on image load
-  $grid.imagesLoaded().progress( function() {
-    $grid.masonry('layout');
-  });
-  // Set gallery
-  $("a.gallery").featherlightGallery();
-  $(".gallery").featherlightGallery({
-		gallery: {
-			fadeIn: 300,
-			fadeOut: 300
-		},
-		openSpeed:    300,
-		closeSpeed:   300
-	});
 });
